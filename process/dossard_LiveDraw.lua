4b1b35d73ecd238415507baec453c36cb84e7336
pguerindon <77529270+pguerindon@users.noreply.github.com>
2022-01-10 11:49:02 +0100

    dossard_LiveDraw.lua version 3
    On ne reconstruit jamais le tableau en rentrant dans le script. Cela doit rester une démarche volontaire.

--- process/dossard_LiveDraw.lua
+++ process/dossard_LiveDraw.lua
@@ -2075,7 +2075,7 @@ function OnAfficheTableau()
 
 	grid_tableau:AddColumnLabel(3);
     grid_tableau:AddRowLabel(1, 48);
-	OnOrder();
+	-- OnOrder();
 
 -- Initialisation des Controles
 	
@@ -2812,7 +2812,7 @@ function main(params_c)
 	draw.height = display:GetSize().height - 30;
 	draw.x = 0;
 	draw.y = 0;
-	draw.version = "2.9";
+	draw.version = "3.0";
 	draw.orderbyCE = 'Rang_tirage, Groupe_tirage, ECSL_points DESC, WCSL_points DESC, ECSL_overall_points DESC, Winner_CC DESC, FIS_pts, Nom, Prenom';
 	draw.orderbyFIS = 'Rang_tirage, Groupe_tirage, FIS_pts, Nom, Prenom';
 	draw.hostname = 'live.fisski.com';
