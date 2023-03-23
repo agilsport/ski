351ddabed6eb3de0dcf866a68364dd35d09ecae7
Philippe Guérindon <philippeguerindon@orange.fr>
2023-03-23 17:17:37 +0100

    Update maj_ressources.lua
    Ce fichier sert à forcer la mise à jour des ressources de PG.

--- process/maj_ressources.lua
+++ process/maj_ressources.lua
@@ -5,19 +5,17 @@ dofile('./interface/adv.lua');
 dofile('./interface/interface.lua');
 
 function main()
-	scrip_version = "1.0"; -- 4.92 pour 2022-2023
-	app.GetAuiFrame():MessageBox(
-		"Vous avez demandé la mise à jour forcée des ressources pour le ski alpin.\nValidez le téléchargement de la mise à jour dans le message suivant.\nElle comprend la totalité des développements spécifiques.", 
-		"Mise à jour des scripts de Ph.Guérindon",
-		msgBoxStyle.OK + msgBoxStyle.ICON_INFORMATION); 
-	if app.GetVersion() >= '5.0' then 
-		-- vérification de l'existence d'une version plus récente du script.
-		-- Ex de retour : LiveDraw=5.94,Matrices=5.92,TimingReport=4.2
-		indice_return = 1;
-		local url = 'https://agilsport.fr/bta_alpin/versionsPG.txt';
-		version = curl.AsyncGET(wnd.GetParentFrame(), url);
+	if app.GetVersion() >= '4.95' then 
+		if app.GetAuiFrame():MessageBox(
+			"Vous avez demandé la mise à jour forcée des ressources pour le ski alpin.\nElle comprend la totalité des développements spécifiques.\nVoulez-vous installer les mises à jour ?", 
+			"Mise à jour des scripts de Ph.Guérindon",
+			msgBoxStyle.YES_NO + msgBoxStyle.NO_DEFAULT + msgBoxStyle.ICON_INFORMATION
+			) == msgBoxStyle.YES then
+			local reponse = app.AutoUpdateResource('https://agilsport.fr/bta_alpin/UpdateScript.zip');
+			-- local url = ' https://agilsport.fr/bta_alpin/UpdateScript.zip';
+			-- Telechargement(url, 'UpdateScript.zip');
+		end
 	end
-	wnd.GetParentFrame():Bind(eventType.CURL, OnCurlReturn);
 	do return end;
 end
 main();
